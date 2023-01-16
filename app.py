e9850ebddc2020537296e4127a3295ab4ab6dc4e
Thales Sobral <thales.sobral@gmail.com>
2023-01-16 17:06:15 -0300

    atualização de código

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 ### Abrindo o dataframe e tratando os dados
 
-tabela = 'fiis_thales_2023-01-13st.xlsx'
+tabela = 'fiis_thales_2023-01-16st.xlsx'
 
 df = pd.read_excel(tabela, sheet_name='FII', usecols='A:V')
 df_completa = df.copy()
